e0d20e5e0b9182447ca0f014b9dadba75c664f18
paulJupilliat <jupilliatp@gmail.com>
2022-10-12 12:26:38 +0200

    fonction correspondance ajouté (Benjamin + Paul)

--- recherche_fichier1.py
+++ recherche_fichier1.py
@@ -1,6 +1,11 @@
+from math import sqrt
 import string
 from fichier1 import message1
 alphabet = string.ascii_uppercase
+
+
+fr_frequence = {"A":8.4,"B":1.06,"C":3.03,"D":4.18,"E":17.26,"F":1.12,"G":1.27,"H":0.92,"I":7.34,"J":0.31,"K":0.05,"L":6.01,"M":2.96,"N":7.13,"O":5.26,"P":3.01,"Q":0.99,"R":6.55,"S":8.08,"T":7.07,"U":5.74,"V":1.32,"W":0.04,"X":0.45,"Y":0.30,"Z":0.12}
+
 
 def clean(s, keep_space=False, keep_case=False):
     """Transforme une chaine en ascii, supprime la ponctuation
@@ -31,7 +36,7 @@ def clean(s, keep_space=False, keep_case=False):
     return s
 print(clean(message1, False, False))
 
-
+#Nous regardons si la methode de cryptage est fréquentielle ou si cela n'a pas de sens.
 def frequences(text):
         """
         fontion qui renvoi la frequence d'apparition des lettres dans un texte passé en parametre
@@ -50,4 +55,32 @@ def frequences(text):
             else : 
                 freq[lettre] = freq[lettre]/len(text)*100
         return freq
-print(frequences(clean(message1, False, False)))
+print(frequences(message1))
+
+
+
+
+
+# grace à ces fonctions, nous trouvons que la lettre la plus utilisé dans le fichier 1 est le V :  9.3 suivi du H: 8.8 et du U : 8.5
+# en comparant ces frequence avec les frenquences des lettres dans le langauge français, cela signifirait que V = E, A = H OU A=U 
+
+def correspondance(dico_frequ_txt, dico_freq_fr):
+    """Fait correspondre chaque lettre à une lettre de la langue française grâce à leur fréquence dans le texte par rapport à la langue fr
+
+    Args:
+        dico_frequ_txt (dict(str: float)): dictionnaire de fréquence d'apparition des lettres dans le texte
+        dico_freq_fr (dict(str: float)): dictionnaire de fréquence d'apparition des lettres dans la langue fr
+    """
+    res = dict()
+    for lettre in dico_frequ_txt:
+        res[lettre] = plus_proche_freq(dico_frequ_txt[lettre], dico_freq_fr)
+    return res
+
+def plus_proche_freq(frequence, dico_freq_langue):
+    min_trouve = None
+    for lettre, freq_lettre in dico_freq_langue.items():
+        if min_trouve is None or abs(frequence - freq_lettre) < min_trouve[1]:
+            min_trouve = (lettre, abs(frequence - freq_lettre))
+    return min_trouve[0]
+
+print(correspondance(frequences(message1),fr_frequence))
