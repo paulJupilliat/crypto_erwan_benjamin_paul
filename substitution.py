41fa854f3a1547c9e860a312cb89ebca1bdd509d
paulJupilliat <jupilliatp@gmail.com>
2022-11-29 15:30:03 +0100

    ajout de fonctions

--- substitution.py
+++ substitution.py
@@ -2,13 +2,23 @@
 
 #on regarde toutes les possibilités et on affiche le message décoder, si ce message comporte les mot d'un dictionnaire français alors on la garde
 #si on a pas de mot correspondant alors on passe a une autre possibilité
-
+def plus_grand_mot(message):
+    mot = ""
+    mot_max = ""
+    for lettre in message:
+        if lettre != " ":
+            mot += lettre
+        else:
+            mot += " "
+            if len(mot)>len(mot_max) or mot_max == "":
+                mot_max = mot
+                mot = ""
+    return mot_max
 
 def creer_decallage(num):
     """fonction permettant de créer deux alphabets en fonction du decallage"""
     i=num
     alphabet_1 = "abcdefghijklmnopqrstuvwxyz"
-    print(alphabet_1)
     alphabet_2 = ""
     for lettre in range(len(alphabet_1)):
         if i < 26:
@@ -25,7 +35,6 @@ def creer_decallage(num):
 
 
 
-
 def equivalence(alphabet_debut, alphabet_2, message):
     for lettre in message:
         if lettre in alphabet_debut:
@@ -35,10 +44,33 @@ def equivalence(alphabet_debut, alphabet_2, message):
     return message
 
 
+def decode(message):
+    mot_fr = open("crypto_erwan_benjamin_paul/fichier_test.txt","r")#open("crypto_erwan_benjamin_paul/french.txt", "r")
+    lignes = mot_fr.readlines()
+    print(lignes)
+    alphabet_debut = "abcdefghijklmnopqrstuvwxyz"
+    #tant que le message decaller n'est pas dans le dictionnaire francais, on decale d'encore 1
+    #peut etre prendre le plus grand mot du message pour verifier dans le dico francais
+    res = True
+    while res:
+        for i in range(26):
+            alphabet_2 = creer_decallage(i)
+            message = equivalence(alphabet_debut, alphabet_2, message)
+            for element in lignes:
+                if plus_grand_mot(message)  == element.strip()+"\n":
+                    res = False
+                    break
+    return message
+
+
 
 message = "YR CNYNVF QR CNHY" #le palais de paul
 
-print(creer_decallage(10))
+
+
+print(plus_grand_mot(message))
+
+print(decode(message))
 
 
     
